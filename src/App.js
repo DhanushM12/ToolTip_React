2112f96f1598861a65724d1e27ef9ac9890c037f
DhanushM12 <dhanushmukhi@gmail.com>
2020-05-07 19:00:25 +0530

    Adding the tooltip component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Tooltip</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super(); //calling the super constructor that is React.Component
+    this.state = {
+      input: "",
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Tooltip</h1>
+        <h2>Enter your input</h2>
+        <label>Click here to Enter</label>
+        <input
+          type="text"
+          value={this.state.value}
+          onInput={this.handleInput}
+        />
+        <ToolTip pos={this.state.input} />
+      </div>
+    );
+  }
 }
 
 export default App;
